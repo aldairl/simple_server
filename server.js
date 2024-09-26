9985e32016a55bbc0e66791d9c216fdb003aa913
aldairl <aldairl@unicauca.edu.co>
2024-09-26 11:58:36 -0500

    feat: url encode

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(express.text())
+app.use(express.urlencoded({ extended: true }))
 
 // Ruta POST
 app.get('/', (req, res) => {
