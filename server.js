8461984565d22e23ca09287568bab752ba654eb2
aldairl <aldairl@unicauca.edu.co>
2024-10-02 15:51:53 -0500

    feat: file upload any field

--- server.js
+++ server.js
@@ -3,27 +3,30 @@ require('dotenv').config();  // Importar dotenv para leer el archivo .env
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const multer = require('multer');
+// const multer = require('multer');
+const fileUpload = require('express-fileupload')
 
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 
 // Middleware para procesar JSON en las solicitudes
 app.use(cors())
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
+app.use(fileUpload())
 
 // Ruta POST
-app.get('/', upload.single('audio'), (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Datos recibidos correctamente', data: 'receivedData' });
 });
 
-app.post('/data', upload.single('audio'), (req, res) => {
+app.post('/data', (req, res) => {
     const receivedData = req.body;
 
-    const {file} = req;
-    console.log('Datos recibidos:', receivedData, "file=", file);
-    res.json({ message: 'Datos recibidos correctamente', data: receivedData });
+    const {files} = req;
+    console.log('Datos recibidos:', receivedData, "file=", files);
+
+    res.json({ message: 'Datos recibidos correctamente', data: receivedData, files });
 });
 
 // Obtener el puerto desde la variable de entorno
