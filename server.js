b3b6790b86eeb7b03a59328a7793cb8f53af5cba
aldairl <aldairl@unicauca.edu.co>
2024-10-03 11:38:46 -0500

    feat: raw header

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const fileUpload = require('express-fileupload')
 app.use(cors())
 app.use(express.json())
 app.use(express.text())
+app.use(express.raw())
 app.use(express.urlencoded({ extended: true }))
 app.use(fileUpload())
 
